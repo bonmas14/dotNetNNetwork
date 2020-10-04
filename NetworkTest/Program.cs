f325ca4f2c6538af569776bcb2091ef494db7415
bonmas14 <bonmas14@gmail.com>
2020-10-04 14:02:55 +0400

    fix

--- NetworkTest/Program.cs
+++ NetworkTest/Program.cs
@@ -7,8 +7,10 @@ namespace NetworkTest
     {
         static void Main(string[] args)
         {
+            // создание экземпляра функции активации
             Sigmoid sigmoid = new Sigmoid();
 
+            // создание набора данных
             double[][] pack = new double[5][];
 
             pack[0] = new double[] { 0.5, 0 };
@@ -17,6 +19,7 @@ namespace NetworkTest
             pack[3] = new double[] { 0.2, 0 };
             pack[4] = new double[] { 1, 0.4 };
             
+            // создание набора для обучения
             double[][] learn = new double[5][];
 
             learn[0] = new double[] { 1, 0 };
@@ -25,25 +28,33 @@ namespace NetworkTest
             learn[3] = new double[] { 0, 0 };
             learn[4] = new double[] { 1, 0 };
 
+            // создание сети
             NNetwork network = new NNetwork(sigmoid, new int[] { 2, 4, 2 });
 
-            for (int i = 0; i < 2000; i++)
+            for (int i = 0; i < 4000; i++)
             {
                 for (int j = 0; j < pack.Length; j++)
                 {
+                    // отправка данных
                     network.SendData(pack[j], true);
+                    
+                    // обновление нейронов
                     network.UpdateData();
+                    
+                    // обучение нейронов
                     network.Learn(learn[j]);
                 }
             }
-
+            // отправка данных
             network.SendData(new double[] { 0.5, 0 }, true);
 
+            // обновление данных
             network.UpdateData();
 
+            // принятие данных
             var data = network.GetOutput();
 
-
+            // вывод данных
             foreach (var item in data)
             {
                 Console.WriteLine(item);
