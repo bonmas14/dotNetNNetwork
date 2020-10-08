d3f095ec088c82d4362684654c54b677ad5e7837
bonmas14 <bonmas14@gmail.com>
2020-10-08 22:00:49 +0400

    to lambda expression

--- NerualNetwork/Sigmoid.cs
+++ NerualNetwork/Sigmoid.cs
@@ -4,14 +4,8 @@ namespace NerualNetwork
 {
     public class Sigmoid : IFunction
     {
-        public double ActivationFunc(double x)
-        {
-            return 1 / (1 + Math.Pow(Math.E, -x));
-        } 
+        public double ActivationFunc(double x) => 1 / (1 + Math.Pow(Math.E, -x));
 
-        public double DeltaFunc(double data)
-        {
-            return data * (1 - data);
-        }
+        public double DeltaFunc(double data)   => data * (1 - data);
     }
 }
