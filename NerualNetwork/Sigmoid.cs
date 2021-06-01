bd44d81471828f2b2cc512735c7f68616439fa44
BonMAS14 <bonmas14@gmail.com>
2021-06-01 19:09:06 +0300

    не значительные изменения

--- NerualNetwork/Sigmoid.cs
+++ NerualNetwork/Sigmoid.cs
@@ -4,8 +4,8 @@ namespace NerualNetwork
 {
     public class Sigmoid : IFunction
     {
-        public double ActivationFunc(double x) => 1 / (1 + Math.Pow(Math.E, -x));
+        public double ActivationFunc(double x) => 1.0f / (1.0f + Math.Pow(Math.E, -x));
 
-        public double DeltaFunc(double data)   => data * (1 - data);
+        public double DeltaFunc(double data)   => data * (1.0f - data);
     }
 }
